99129c8a7efba6d3c3bfce3b5cf2932c505092d4
VladislavLavrinovich1994 <markvimand@gmail.com>
2019-11-02 01:08:55 +0300

    Updates

---
+++ precache-manifest.1200c83c8f32ebfa577074dd46735053.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bde68ee8eee825aab44c3a871528db8",
+    "url": "/TodoListReact/index.html"
+  },
+  {
+    "revision": "4a208c320eba75732784",
+    "url": "/TodoListReact/static/css/main.c0c14cb7.chunk.css"
+  },
+  {
+    "revision": "58f383c7f23ac47a1424",
+    "url": "/TodoListReact/static/js/2.af2decc0.chunk.js"
+  },
+  {
+    "revision": "4a208c320eba75732784",
+    "url": "/TodoListReact/static/js/main.86ad218c.chunk.js"
+  },
+  {
+    "revision": "5b33908ad625df18ce82",
+    "url": "/TodoListReact/static/js/runtime~main.b5a25205.js"
+  }
+]);
